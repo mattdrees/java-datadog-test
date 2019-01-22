ca350a0d848efcc6e6d555a5ed85928e780538d9
Matt Drees <matt.drees@cru.org>
2019-01-22 12:50:21 -0700

    Initial commit from archetype
    Built from archetype:generate interactively, choosing "internal -> org.apache.maven.archetypes:maven-archetype-quickstart (An archetype which contains a sample Maven project.)"

---
+++ src/main/java/org/cru/tracing/ddtest/App.java
@@ -0,0 +1,13 @@
+package org.cru.tracing.ddtest;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
