ebd0785493ea1b4ba39bd42576e730448625efe8
Matt Drees <matt.drees@cru.org>
2019-01-23 12:07:03 -0700

    Set up nicer looking test data

--- src/main/java/org/cru/tracing/ddtest/App.java
+++ src/main/java/org/cru/tracing/ddtest/App.java
@@ -1,8 +1,10 @@
 package org.cru.tracing.ddtest;
 
+import datadog.trace.api.Trace;
 import io.opentracing.Scope;
 import io.opentracing.Tracer;
 import io.opentracing.util.GlobalTracer;
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -11,21 +13,37 @@ import java.util.concurrent.TimeUnit;
  */
 public class App 
 {
+    static Tracer tracer = GlobalTracer.get();
+    static Random random = new Random();
+
+    static int count = 0;
     public static void main( String[] args ) throws InterruptedException {
-        int count = 0;
         while (true) {
-            Tracer tracer = GlobalTracer.get();
 
-            try (Scope scope = tracer.buildSpan("printing").startActive(true)) {
+            try (Scope scope = tracer.buildSpan("loop").startActive(true)) {
                 scope.span().setTag("count", count);
-
-                System.out.println( "Hello World!" );
+                print();
+                sleepShort();
             }
-            try (Scope scope = tracer.buildSpan("sleeping").startActive(true)) {
-                scope.span().setTag("count", count);
+            sleepLong();
+            count++;
+        }
+    }
 
-                TimeUnit.SECONDS.sleep(5);
-            }
+//    @Trace
+    private static void sleepLong() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(5);
+    }
+
+    @Trace
+    private static void sleepShort() throws InterruptedException {
+        TimeUnit.MICROSECONDS.sleep(random.nextInt(1000));
+    }
+
+    @Trace
+    private static void print() {
+        try (Scope scope = tracer.buildSpan("printing").startActive(true)) {
+            System.out.println( "Hello World!" );
         }
     }
 }
