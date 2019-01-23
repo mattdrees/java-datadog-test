168dfce85ace08256ddacab84916b43fe7e4a378
Matt Drees <matt.drees@cru.org>
2019-01-23 10:21:44 -0700

    Add basic apm tracing

--- src/main/java/org/cru/tracing/ddtest/App.java
+++ src/main/java/org/cru/tracing/ddtest/App.java
@@ -1,5 +1,8 @@
 package org.cru.tracing.ddtest;
 
+import io.opentracing.Scope;
+import io.opentracing.Tracer;
+import io.opentracing.util.GlobalTracer;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -9,9 +12,20 @@ import java.util.concurrent.TimeUnit;
 public class App 
 {
     public static void main( String[] args ) throws InterruptedException {
+        int count = 0;
         while (true) {
-            System.out.println( "Hello World!" );
-            TimeUnit.SECONDS.sleep(5);
+            Tracer tracer = GlobalTracer.get();
+
+            try (Scope scope = tracer.buildSpan("printing").startActive(true)) {
+                scope.span().setTag("count", count);
+
+                System.out.println( "Hello World!" );
+            }
+            try (Scope scope = tracer.buildSpan("sleeping").startActive(true)) {
+                scope.span().setTag("count", count);
+
+                TimeUnit.SECONDS.sleep(5);
+            }
         }
     }
 }
